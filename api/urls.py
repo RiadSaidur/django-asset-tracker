185f60bc5bf89f16d14cd095927662d0bb82229f
RiadSaidur <reidvai1998@gmail.com>
2023-04-14 00:56:33 +0600

    added: employee add

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('list-company/', views.ListCompany, name='list-company'),
   path('list-employee/', views.ListEmployee, name='list-employee'),
+  path('add-employee/', views.AddEmployee, name='add-employee'),
   path('list-asset/', views.ListAsset, name='list-asset'),
 ]
--- api/views.py
+++ api/views.py
@@ -17,6 +17,16 @@ def ListEmployee(request):
   serializer = EmployeeSeiralizer(employees, many=True)
   return Response(serializer.data)
 
+@api_view(['POST'])
+def AddEmployee(request):
+  serializer = EmployeeSeiralizer(data=request.data)
+
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data)
+
+  return Response({ 'error': 'Employee add failed unexpectedy!' })
+
 
 @api_view(['GET'])
 def ListAsset(request):
